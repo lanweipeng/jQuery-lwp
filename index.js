08a3a60931c1619faab2e96e8bdeaff144680fec
兰为鹏 <1462191066@qq.com>
2019-12-08 00:25:41 +0800

    onload

--- index.js
+++ index.js
@@ -1,106 +1,24 @@
 var element
 var $ = function (select) {
-  if (select === undefined) {
+  if(typeof select === 'function'){
+    window.onload=select()
+  }
+  else if (select === undefined) {
+
     return
   }
-  if (select instanceof Node) {
+ else if (select instanceof Node) {
     element = select
     return $
   } else if (select.substr(0, 1) == '#') {
     return document.querySelector(select)
+  }else{
+    return document.querySelectorAll(select)
+
   }
-  return document.querySelectorAll(select)
 }
 
-var EventUtil = {
-  addHandler: function (element, type, handler) {
-    if (element.addEventListener) {
-      element.addEventListener(type, handler, false)
-    } else if (element.attachEvent) {
-      element.attachEvent('on' + type, handler)
-    } else {
-      element['on' + type] = handler
-    }
-  },
-  getEvent: function (event) {
-    return event ? event : window.event
-  },
-  getTarget: function (event) {
-    return event.target || event.srcElement
-  },
-  preventDefault: function (event) {
-    if (event.preventDefault) {
-      event.preventDefault()
-    } else {
-      event.returnValue = false
-    }
-  },
-  removeHandle: function (element, type, handler) {
-    if (element.removeEventListener) {
-      element.addEventListener(type, handler, false)
-    } else if (element.detachEvent) {
-      element.detachEvent('on' + type, handler)
-    } else {
-      element['on' + type] = null
-    }
-  },
-  stopPropagation: function (event) {
-    if (event.stopPropagation) {
-      event.stopPropagation()
-    } else {
-      event.cancelBubble = true
-    }
-  }
-}
-$.addHandler = function (element, type, handler) {
-  if (element.addEventListener) {
-    element.addEventListener(type, handler, false)
-  } else if (element.attachEvent) {
-    element.attachEvent('on' + type, handler)
-  } else {
-    element['on' + type] = handler
-  }
-}
-$.getEvent = function (event) {
-  return event ? event : window.event
-}
-$.getTarget = function (event) {
-  return event.target || event.srcElement
-}
-$.preventDefault = function (event) {
-  if (event.preventDefault) {
-    event.preventDefault()
-  } else {
-    event.returnValue = false
-  }
-}
-$.removeHandle = function (element, type, handler) {
-  if (element.removeEventListener) {
-    element.addEventListener(type, handler, false)
-  } else if (element.detachEvent) {
-    element.detachEvent('on' + type, handler)
-  } else {
-    element['on' + type] = null
-  }
-}
-$.stopPropagation = function (event) {
-  if (event.stopPropagation) {
-    event.stopPropagation()
-  } else {
-    event.cancelBubble = true
-  }
-}
-$.getRelatedTarget = function (event) {
-  if (event.relatedTarget) {
-    return event.relatedTarget
-  } else if (event.toElement) {
-    return event.toElement
-  } else if (event.fromElement) {
-    return event.fromElement
-  } else {
-    return null
-  }
-}
+
 $.getButton = function (event) {
   if (document.implementation.hasFeature('MouseEvents', '2.0')) {
     return event.button
@@ -136,4 +54,8 @@ $.setClipboardText=function(event,value){
   }else if(window.clipboardData){
     return window.clipboardData.setData('text',value)
   }
+}
+function $(body){
+  console.log(body)
+// window.onload=body()
 }
