2511a4f042ee7bed0be35a93ccb381fd2e590080
兰为鹏 <1462191066@qq.com>
2020-07-20 12:44:41 +0800

    公司电脑

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var element
+var s
 var $ = function (select) {
   if(typeof select === 'function'){
     window.onload=select()
@@ -11,11 +12,13 @@ var $ = function (select) {
     element = select
     return $
   } else if (select.substr(0, 1) == '#') {
+    s=document.querySelector(select)
     return document.querySelector(select)
   }else{
     return document.querySelectorAll(select)
 
   }
+  return this
 }
 
 
@@ -55,7 +58,30 @@ $.setClipboardText=function(event,value){
     return window.clipboardData.setData('text',value)
   }
 }
+$.css=function(x,y){
+console.log(s)
+}
+const JQ=function(){};
+JQ.prototype={
+  css:function(x,y){
+    console.log(s)
+    }
+}
+var jq=new JQ();
 function $(body){
   console.log(body)
 // window.onload=body()
 }
+var jQuery=function(selector){
+  return new jQuery.fn.init(selector);
+};
+jQuery.fn=jQuery.prototype={
+
+}
+var init=jQuery.fn.init=function(selector){
+  var elem=document.getElementById(selector.split('#')[1]);
+  this[0]=elem;
+  this.length=1
+  return this
+}
+init.prototype=jQuery.fn;
