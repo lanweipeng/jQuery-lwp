e295af1b569d491119872c1fc94fab5b96cd9ce9
lanweipeng <1462191066@qq.com>
2020-09-20 20:57:52 +0800

    颠倒

--- index.js
+++ index.js
@@ -1,78 +1,3 @@
-var element
-
-
-
-var $ = function (select) {
-  if (typeof select === 'function') {
-    window.onload = select()
-  }
-  else if (select === undefined) {
-
-    return
-  }
-  else if (select instanceof Node) {
-    element = select
-    return $
-  } else if (select.substr(0, 1) == '#') {
-    s = document.querySelector(select)
-    return document.querySelector(select)
-  } else {
-    return document.querySelectorAll(select)
-
-  }
-  return this
-}
-
-
-$.getButton = function (event) {
-  if (document.implementation.hasFeature('MouseEvents', '2.0')) {
-    return event.button
-  } else {
-    switch (event.button) {
-      case 7: return 0
-      case 6: return 2
-      case 4: return 1
-    }
-  }
-}
-$.getCharCode = function (event) {
-  if (typeof event.charCode == 'number') {
-    return event.charCode
-  } else {
-    return event.keyCode
-  }
-}
-$.addClass = function (string) {
-  element.classList.add(string)
-}
-$.removeClass = function (string) {
-  element.classList.remove(string)
-}
-
-$.getClipboardText = function (event) {
-  var clipboardData = (event.clipboardData || window.clipboardData)
-  return clipboardData.getData('text')
-}
-$.setClipboardText = function (event, value) {
-  if (event.clipboardData) {
-    return event.clipboardData.setData('text/plian', value)
-  } else if (window.clipboardData) {
-    return window.clipboardData.setData('text', value)
-  }
-}
-$.css = function (x, y) {
-  console.log(s)
-}
-
-
-toString = Object.prototype.toString;
-
-
-
-
-
-
-
 (function (window) {
   var jQuery = (function () {
     var class2type = {},
@@ -353,3 +278,79 @@ toString = Object.prototype.toString;
   })()
   window.$ = window.jQuery = jQuery;
 })(window)
+
+
+var element
+
+
+
+var $ = function (select) {
+  if (typeof select === 'function') {
+    window.onload = select()
+  }
+  else if (select === undefined) {
+
+    return
+  }
+  else if (select instanceof Node) {
+    element = select
+    return $
+  } else if (select.substr(0, 1) == '#') {
+    s = document.querySelector(select)
+    return document.querySelector(select)
+  } else {
+    return document.querySelectorAll(select)
+
+  }
+  return this
+}
+
+
+$.getButton = function (event) {
+  if (document.implementation.hasFeature('MouseEvents', '2.0')) {
+    return event.button
+  } else {
+    switch (event.button) {
+      case 7: return 0
+      case 6: return 2
+      case 4: return 1
+    }
+  }
+}
+$.getCharCode = function (event) {
+  if (typeof event.charCode == 'number') {
+    return event.charCode
+  } else {
+    return event.keyCode
+  }
+}
+$.addClass = function (string) {
+  element.classList.add(string)
+}
+$.removeClass = function (string) {
+  element.classList.remove(string)
+}
+
+$.getClipboardText = function (event) {
+  var clipboardData = (event.clipboardData || window.clipboardData)
+  return clipboardData.getData('text')
+}
+$.setClipboardText = function (event, value) {
+  if (event.clipboardData) {
+    return event.clipboardData.setData('text/plian', value)
+  } else if (window.clipboardData) {
+    return window.clipboardData.setData('text', value)
+  }
+}
+$.css = function (x, y) {
+  console.log(s)
+}
+
+
+toString = Object.prototype.toString;
+
+
+
+
+
+
